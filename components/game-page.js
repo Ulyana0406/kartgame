79b9af89426f76d1cb0495ff2c0049e7bd5a4047
Ulyana0406 <yliana0406@mail.ru>
2023-08-28 14:46:10 +0300

    Some problems with lint

--- components/game-page.js
+++ components/game-page.js
@@ -1,19 +1,19 @@
 export function renderGamePage(difficulty, appEl) {
-    let gameHtml = '';
-    
-    if (difficulty === "easy") {
-      gameHtml = `
+    let gameHtml = ''
+
+    if (difficulty === 'easy') {
+        gameHtml = `
         <div class="page__title">ТУТ БУДЕТ ЛЕГКАЯ СЛОЖНОСТЬ</div>
-      `;
-    } else if (difficulty === "medium") {
-      gameHtml = `
+      `
+    } else if (difficulty === 'medium') {
+        gameHtml = `
         <div class="page__title">ТУТ БУДЕТ СРЕДНЯЯ СЛОЖНОСТЬ</div>
-      `;
-    } else if (difficulty === "hard") {
-      gameHtml = `
+      `
+    } else if (difficulty === 'hard') {
+        gameHtml = `
         <div class="page__title">ТУТ БУДЕТ БОЛЬШАЯ СЛОЖНОСТЬ</div>
-      `;
+      `
     }
-    
-    appEl.innerHTML = gameHtml;
-  };
+
+    appEl.innerHTML = gameHtml
+}
--- modules/routes.js
+++ modules/routes.js
@@ -1,4 +1,4 @@
-export const EASY_PAGE = "easy-page";
-export const MEDIUM_PAGE = "medium-page";
-export const HARD_PAGE = "hard-page";
-export const FIRST_PAGE = "first-page";
+export const EASY_PAGE = 'easy-page'
+export const MEDIUM_PAGE = 'medium-page'
+export const HARD_PAGE = 'hard-page'
+export const FIRST_PAGE = 'first-page'
