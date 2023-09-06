f00a3fb0a7e6dbd6ed2ee443205acdc650c341c6
Ulyana0406 <yliana0406@mail.ru>
2023-09-06 15:23:07 +0300

    Все проблемы исправлены, задания выполнены

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-//const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 module.exports = {
     entry: '/main.js',
     mode: 'production',
@@ -10,21 +12,25 @@ module.exports = {
         clean: true,
     },
     plugins: [
-        // new CopyPlugin({
-        //    patterns: [{ from: 'static', to: 'static' }],
-        // }),
+        new CopyPlugin({
+            patterns: [{ from: 'cards', to: 'cards' }],
+        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './index.html',
         }),
+        new MiniCssExtractPlugin(),
     ],
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
                 use: [
+                    MiniCssExtractPlugin.loader,
                     // Creates `style` nodes from JS strings
-                    'style-loader',
                     // Translates CSS into CommonJS
                     'css-loader',
                     // Compiles Sass to CSS
